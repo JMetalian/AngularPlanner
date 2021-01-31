c2d9a67c123d119067c7ceb3b10a02f788f0348a
Canberk Ardıç <canberk.ardic@cantekgroup.com>
2021-01-31 21:45:51 +0300

    Development : Providers Component Implemented

--- src/app/components/providers/providers.component.ts
+++ src/app/components/providers/providers.component.ts
@@ -10,6 +10,28 @@ export class ProvidersComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  
+  }
+
+  textValue = "";
+  providers = [
+    {
+      "name" : "Netflix",
+      "date" : "2020",
+      "time" : "20:30"
+    },
+    {
+      "name" : "ORF",
+      "date" : "2020",
+      "time" : "17:30"
+    }
+  ]
+
+  selectedProvider : any;
+
+  onAddButtonClicked(){
+    //this.appService.getSearchData(this.textValue);
+    console.log("Bana tıklandı")
   }
 
 }
--- src/shared/primeng/primeng.module.ts
+++ src/shared/primeng/primeng.module.ts
@@ -2,11 +2,14 @@ import {  NgModule } from '@angular/core';
 import {InputTextModule} from 'primeng/inputtext';
 import { SidebarModule} from 'primeng/sidebar';
 import {TableModule} from 'primeng/table';
+import {DropdownModule} from 'primeng/dropdown';
+
 
 const PRIMENG_MODULES = [
   InputTextModule,
   SidebarModule,
-  TableModule
+  TableModule,
+  DropdownModule
 ];
 
 @NgModule({
